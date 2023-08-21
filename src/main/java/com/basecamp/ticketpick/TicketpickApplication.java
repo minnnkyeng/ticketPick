04203df24e0addc2f4d73f21943fdb12645c93b3
minnnkyeng <minnn.xyz@gmail.com>
2023-08-22 01:20:08 +0900

    second commit

---
+++ src/main/java/com/basecamp/ticketpick/TicketpickApplication.java
@@ -0,0 +1,13 @@
+package com.basecamp.ticketpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basecamp/ticketpick/TicketpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basecamp.ticketpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
