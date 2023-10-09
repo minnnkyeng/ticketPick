d39259f54a38c79c82efe3e9676677fa7e096247
minnnkyeng <minnn.xyz@gmail.com>
2023-10-09 20:41:35 +0900

    refactor : Slf4j 적용
    - Slf4j setting
    - Controller @Slf4j 적용
    
    * Files
    ProductController.java
    application.properties
    build.gradle

--- src/main/java/com/basecamp/ticketpick/product/ProductController.java
+++ src/main/java/com/basecamp/ticketpick/product/ProductController.java
@@ -1,5 +1,6 @@
 package com.basecamp.ticketpick.product;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Slf4j
 public class ProductController {
     private final ProductService productService;
 
@@ -17,33 +19,43 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<String> findAllProduct() {
+        log.info("[ProductController] findAllProduct() START" );
         List<Product> products = productService.findAllProduct();
         if(products.isEmpty()){
             return ResponseEntity.ok("no products.");
         }
+        log.info("[ProductController] findAllProduct() END" );
         return ResponseEntity.ok(products.toString());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> findProduct(@PathVariable int id) {
+        log.info("[ProductController] findProduct(int) START" );
+        log.info("[ProductController] findProduct(int), id={}", id);
         Product product = productService.findProduct(id);
         if(product==null){
             return ResponseEntity.ok("There is not product, id="+id);
         }
+        log.info("[ProductController] findAllProduct() END" );
         return ResponseEntity.ok(product);
     }
 
     @PostMapping
     public ResponseEntity<?> saveProduct(@RequestBody Product product) {
+        log.info("[ProductController] saveProduct(Product) START" );
+        log.info("[ProductController] saveProduct(Product), product={}", product);
         if(product==null){
             return ResponseEntity.badRequest().body("Request data is null.");
         }
         int id = productService.saveProduct(product);
+        log.info("[ProductController] saveProduct(Product) END" );
         return ResponseEntity.created(URI.create("/products/"+id)).build();
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestBody ProductRequestDTO productRequestDTO) {
+        log.info("[ProductController] updateProduct(int, ProductRequestDTO) START" );
+        log.info("[ProductController] updateProduct(int, ProductRequestDTO), id={}, productRequestDTO={}", id, productRequestDTO);
         Product product = productService.findProduct(id);
         if(productRequestDTO==null){
             return ResponseEntity.badRequest().body("Request data is null.");
@@ -52,11 +64,14 @@ public class ProductController {
             return ResponseEntity.badRequest().body("There is not product, id="+id);
         }
         productService.updateProduct(product, productRequestDTO);
+        log.info("[ProductController] findAllProduct() END" );
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable int id) {
+        log.info("[ProductController] deleteProduct(int) START" );
+        log.info("[ProductController] deleteProduct(int), id={}", id);
         Product product = productService.findProduct(id);
         if(product==null){
             return ResponseEntity.ok("There is not product, id="+id);
@@ -65,6 +80,7 @@ public class ProductController {
             return ResponseEntity.badRequest().body("Product id is under 1");
         }
         productService.deleteProduct(id);
+        log.info("[ProductController] deleteProduct(int) END" );
         return ResponseEntity.noContent().build();
     }
 }
