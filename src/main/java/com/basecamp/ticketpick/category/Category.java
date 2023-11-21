1e04ca58f2bd39d64c62fdeb3973e06e49e5998f
minnnkyeng <minnn.xyz@gmail.com>
2023-11-21 23:20:45 +0900

    refactor : 롬복 추가 적용
    * files
    Product.java
    Category.java

--- src/main/java/com/basecamp/ticketpick/category/Category.java
+++ src/main/java/com/basecamp/ticketpick/category/Category.java
@@ -1,38 +1,20 @@
 package com.basecamp.ticketpick.category;
 
-import com.basecamp.ticketpick.product.Product;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 @Table(name = "categories")
+@Setter
+@Getter
+@ToString
 public class Category {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
-
-    public int getId() { return id; }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/basecamp/ticketpick/product/Product.java
+++ src/main/java/com/basecamp/ticketpick/product/Product.java
@@ -1,11 +1,19 @@
 package com.basecamp.ticketpick.product;
 
 import com.basecamp.ticketpick.category.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name="products")
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
 public class Product {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -15,7 +23,7 @@ public class Product {
     private String date;
     private String venue;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
 
@@ -33,84 +41,5 @@ public class Product {
         this.date = date;
         this.venue = venue;
         this.category = category;
-    }
-
-    public Product(int id, String name, String description, int price, String date, String venue, Category category) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.date = date;
-        this.venue = venue;
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getVenue() {
-        return venue;
-    }
-
-    public void setVenue(String venue) {
-        this.venue = venue;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", date='" + date + '\'' +
-                ", venue='" + venue + '\'' +
-                ", category=" + category +
-                '}';
     }
 }
